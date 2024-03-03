737e0f776c2195b81b4acb1e1396b5aba7bc1691
Nikita <dimavlaskin9@gmail.com>
2024-03-03 23:25:51 +0300

    inital commit

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header />
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Header from '~/components/elements/Header.vue';
+
+</script>
+
+<style>
+body {
+  background-color: #020420;
+  color: white;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLayout>
+    <div>
+      Defaul layout hello!
+    </div>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
